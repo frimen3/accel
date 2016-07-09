923c5133f93fa37c95b2842912e3e20ba8b44026
frimen3 <frimen3@gmail.com>
2016-07-09 14:10:23 +0300

    Update neoway.ino

--- neoway/neoway.ino
+++ neoway/neoway.ino
@@ -16,7 +16,7 @@ void loop() {
         Serial.println("error 1");
         delay(100);  
       }
-      gsm.println("ATD+79217025761;"); // звоним по указаному номеру
+      gsm.println("ATD+79210000000;"); // звоним по указаному номеру
       delay(100);
       //if (gsm.find("OK")) Serial.println("OK!");
       //else Serial.println("error 2");
